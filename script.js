7bdd9e03706fb632b9c786e0e8ed1ec9b576a50a
kadirhanozdemir <kadirhanozdemir95@gmail.com>
2025-07-17 21:41:52 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const music = document.getElementById("bg-music");
-    music.volume = 0.5;
-    music.play().catch(e => {
-        console.log("Ses çalmadı, kullanıcı etkileşimi gerekli olabilir.");
-    });
-
-    // Buraya zambak animasyonu ve havai fişek efektleri eklenebilir
-});
